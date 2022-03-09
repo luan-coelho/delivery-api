6dbd9ce74ff890d0719e626c5bfd715af2b2f9e4
luan-coelho <luancoelho.dev@gmail.com>
2022-03-09 17:27:58 -0300

    client repository implementation

--- src/main/java/com/study/delivery/api/controller/ClientController.java
+++ src/main/java/com/study/delivery/api/controller/ClientController.java
@@ -1,5 +1,28 @@
 package com.study.delivery.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.study.delivery.api.model.Client;
+import com.study.delivery.api.repository.ClientRepository;
+
+@RestController
 public class ClientController {
 
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@GetMapping("/clients")
+	public List <Client> findAll(){
+		return clientRepository.findAll();
+	}
+	
+	@GetMapping("/clients/{name}")
+	public List <Client> findByName(@PathVariable String name){
+		return clientRepository.findByNameContaining(name);
+	}
 }
--- src/main/java/com/study/delivery/api/model/Client.java
+++ src/main/java/com/study/delivery/api/model/Client.java
@@ -1,14 +1,27 @@
 package com.study.delivery.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Client {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
 	private String phone;
+	
 }
