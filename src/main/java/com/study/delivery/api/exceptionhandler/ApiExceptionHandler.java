12a500941f83c640deca98f35fb7736a6248a6ea
luan-coelho <luancoelho.dev@gmail.com>
2022-03-12 10:16:47 -0300

    creating messages file with custom messages

--- src/main/java/com/study/delivery/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/study/delivery/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +17,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.Getter;
+
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Getter
+	@Autowired
+	private MessageSource messageSource;
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -25,7 +34,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
 			String name = ((FieldError) error).getField();
-			String message = error.getDefaultMessage();
+			String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 			
 			fields.add(new Field(name, message));
 		}
